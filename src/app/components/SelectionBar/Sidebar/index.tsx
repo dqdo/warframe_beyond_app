c02aeafc8121b8677ad3e60cb13f301a402c5e43
dqdo <dqdo873@gmail.com>
2025-05-16 23:40:43 -0400

    Made sidebar dimensions dynamic

--- src/app/components/SelectionBar/Sidebar/index.tsx
+++ src/app/components/SelectionBar/Sidebar/index.tsx
@@ -6,7 +6,7 @@ type SidebarProps = {
 export default function Sidebar({ type, isOpen }: SidebarProps) {
     return (
         <div
-            className={`fixed right-0 w-150 h-215 text-white bg-neutral-900 border-l border-b border-white border-t-0 border-r-0 transform transition-transform duration-400 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+            className={`fixed right-0 w-[35vw] h-[90vh] text-white bg-neutral-900 border-l border-b border-white border-t-0 border-r-0 transform transition-transform duration-400 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}
         >
             <h2 className="p-4 font-bold">{type}</h2>
         </div>
